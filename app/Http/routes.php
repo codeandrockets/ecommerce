9148be4adacb355c185becc129ba3a7a4340f61e
Steven Jay <stevenjasionowicz@gmail.com>
2016-03-18 15:26:41 +1300

    Add Authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -34,3 +34,9 @@ Route::resource('about', 'AboutController', ['only' => ['index']]);
 Route::group(['middleware' => ['web']], function () {
     //
 });
+
+Route::group(['middleware' => 'web'], function () {
+    Route::auth();
+
+    Route::get('/home', 'HomeController@index');
+});
